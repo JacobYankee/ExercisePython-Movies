556173e20a37c1381eb39c25c60a87376d15cc9e
JacobYankee <jmyankee3@gmail.com>
2023-11-01 10:48:22 -0400

    Create simple menu
    Fix: add basic menu functionality
    
    Added a simple menu functionality that can be quit from.
    
    Issue #1

--- query.py
+++ query.py
@@ -1,3 +1,11 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+choice = ""
+while choice != "q":
+    print("Menu options: \n q: quit")
+    choice = input()
+    if choice =="q":
+        print("exiting menu")
+        break
