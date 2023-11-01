0030f743b2e60a6503fa338754550648dc8b918f
JacobYankee <jmyankee3@gmail.com>
2023-11-01 10:50:21 -0400

    Update menu with list functionality
    Fix: add list functionality
    
    Added ability to view list of movies in the menu.
    
    Issue #2

--- query.py
+++ query.py
@@ -3,9 +3,18 @@ from movies import Movies
 movies = Movies('./movies.txt')
 
 choice = ""
+enc = 'utf-8'
+with open("movies.txt", "r", encoding=enc) as data:
+    list = data.readlines()
 while choice != "q":
-    print("Menu options: \n q: quit")
+    print("Menu options: \n q: quit \n list: list all movies")
     choice = input()
     if choice =="q":
         print("exiting menu")
         break
+    elif choice == "list":
+        row_idx = 0
+        for line in list:
+            if row_idx%3==0:
+                print(line, end="")
+            row_idx+=1
