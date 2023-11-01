582eac585ae3f57882a2c421ff60d22db38c00fb
JacobYankee <jmyankee3@gmail.com>
2023-11-01 10:51:52 -0400

    Update menu with movie search functionality
    Fix: add search functionality
    
    Added ability to search for movies based off keyword.
    
    Issue #3

--- query.py
+++ query.py
@@ -7,7 +7,7 @@ enc = 'utf-8'
 with open("movies.txt", "r", encoding=enc) as data:
     list = data.readlines()
 while choice != "q":
-    print("Menu options: \n q: quit \n list: list all movies")
+    print("Menu options: \n q: quit \n list: list all movies \n sn: search movie names")
     choice = input()
     if choice =="q":
         print("exiting menu")
@@ -18,3 +18,12 @@ while choice != "q":
             if row_idx%3==0:
                 print(line, end="")
             row_idx+=1
+    elif choice =="sn":
+        print("Enter a word to search: ")
+        word = input()
+        row_idy = 0
+        for line in list:
+            if row_idy%3 == 0:
+                if word.lower() in line.lower():
+                    print(line, end="");
+            row_idy += 1
